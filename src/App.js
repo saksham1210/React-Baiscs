657f5a98ca70dd74f707e8e1be9a6bf3023e9f46
Saksham Dhull <saksham18186@iiitd.ac.in>
2020-06-06 17:20:42 +0530

    remove styled components from app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from "./Person/Person";
-import styled from 'styled-components';
+
+// import styled from 'styled-components';
+
+// const StyledButton=styled.button`
+//   background-color: ${props => props.alt ? 'red' : 'green'};
+//   color:white;
+//   font:inherit;
+//   border:1px solid blue;
+//   padding:8px;
+//   cursor:pointer;
+//   &:hover{
+//     background-color:${props => props.alt ? 'salmon' : 'lightgreen'};
+//     color:black;
+//   }
+// `;
 
 
-const StyledButton=styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color:white;
-  font:inherit;
-  border:1px solid blue;
-  padding:8px;
-  cursor:pointer;
-  &:hover{
-    background-color:${props => props.alt ? 'salmon' : 'lightgreen'};
-    color:black;
-  }
-`;
 class App extends Component {
 
   state = {
@@ -52,18 +54,18 @@ class App extends Component {
   }
 
   render() {
-    // const style={
-    //   backgroundColor:'green',
-    //   color:'white',
-    //   font:'inherit',
-    //   border:'1px solid blue',
-    //   padding:'8px',
-    //   cursor:'pointer',
-    //   ':hover':{
-    //     backgroundColor:'lightgreen',
-    //     color:'black'
-    //   }
-    // }
+    const style={
+      backgroundColor:'green',
+      color:'white',
+      font:'inherit',
+      border:'1px solid blue',
+      padding:'8px',
+      cursor:'pointer',
+      ':hover':{
+        backgroundColor:'lightgreen',
+        color:'black'
+      }
+    }
     let persons=null;
     if (this.state.showPersons){
       persons=(
@@ -79,11 +81,11 @@ class App extends Component {
           })}
         </div> 
       );
-      // style.backgroundColor="red";
-      // style[':hover']={
-      //   backgroundColor:'salmon',
-      //   color:'black'
-      // }
+      style.backgroundColor="red";
+      style[':hover']={
+        backgroundColor:'salmon',
+        color:'black'
+      }
 
     }
     let classes =[];
@@ -97,9 +99,9 @@ class App extends Component {
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
-          <StyledButton alt={this.state.showPersons} onClick={this.togglePersonHandler}>
+          <button style={style} alt={this.state.showPersons} onClick={this.togglePersonHandler}>
               Toggle persons
-          </StyledButton>
+          </button>
           {persons}
         </div>      
     );
