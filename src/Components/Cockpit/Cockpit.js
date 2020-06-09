64cbeea82349ae1d9c553d97292280c3b4776aa8
Saksham Dhull <saksham18186@iiitd.ac.in>
2020-06-10 00:55:32 +0530

    paased prop to app component

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -19,7 +19,7 @@ const cockpit =(props) =>{
 
     return (
         <div className={classes.Cockpit}>
-            <h1>Hi, I'm a React App</h1>
+            <h1>{props.title}</h1>
             <p className={assignedclasses.join(' ')}>This is really working!</p>
             <button className={btnClass} alt={props.showPersons} onClick={props.clicked}>
                 Toggle persons
