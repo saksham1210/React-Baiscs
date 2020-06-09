2e7a7b0438a5d63000b1db18b71324f89bf6892a
Saksham Dhull <saksham18186@iiitd.ac.in>
2020-06-10 01:36:53 +0530

    changed person/persons components to class-based components

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,32 +1,16 @@
-import React from 'react';
+import React,{Component} from 'react';
 import classes from './Person.css';
-// import styled from 'styled-components';
-// const StyledDiv = styled.div`
-//     width:60%;
-//     margin:16px auto;
-//     border:1px solid #eeeeee;
-//     box-shadow:0 2px 3px #cccccc;
-//     padding: 16px;
-//     text-align: center;
-//     @media (min-width:500px){
-//         width:450px
-//     }
-//     `;
-const person = (props) => {
-    // const style={
-    //     '@media (min-width:500px)': {
-    //         width:'450px'
-    //     }
-    // };
-    console.log("[Person.js] rendering ...")
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type='text' onChange={props.changed} value={props.name}></input>
-        </div>
-    )
+
+class Person extends Component {
+    render(){
+        console.log("[Person.js] rendering ...");
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type='text' onChange={this.props.changed} value={this.props.name}></input>
+            </div>
+        );
+    }
 };
-
-
-export default person;
+export default Person;
